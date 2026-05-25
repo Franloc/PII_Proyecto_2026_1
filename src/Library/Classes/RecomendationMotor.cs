a078ae6a69eb63b1a9437fa51b5a29ce19f1bf11
Franloc <francesco.locatelli@correo.ucu.edu.uy>
2026-05-25 09:30:18 -0300

    Motor

--- src/Library/Classes/RecomendationMotor.cs
+++ src/Library/Classes/RecomendationMotor.cs
@@ -8,11 +8,38 @@ namespace Library
         {
             get {return this.recommendationStrategies;} set{this.recommendationStrategies = value;}
         }
-
-        //Constructor
-        public RecommendationEngine(List<IRecommendationStrategy> estrategias)
+        private List<IFilter> filters;
+        public List<IFilter> Filters
         {
-            this.recommendationStrategies = estrategias;
+            get {return this.filters;} set {this.filters = value;}
+        }
+        private IRankingStrategy rankingStrategy;
+        public IRankingStrategy RankingStrategy
+        {
+            get {return this.rankingStrategy;} set {this.rankingStrategy = value;}
+        }
+        public List<IMedia> Recommend(User user, List<IMedia> media)
+        {
+            List<IMedia> recommendations = new List<IMedia>();
+            foreach (IRecommendationStrategy strategy in this.recommendationStrategies)
+            {
+                recommendations.AddRange(
+                strategy.Recommend(user, media));
+            }
+            recommendations = recommendations.Distinct().ToList();
+            foreach (IFilter filter in this.filters)
+            {
+                recommendations = filter.Filter(user, recommendations);
+            }
+            recommendations = this.rankingStrategy.Rank(user, recommendations);
+            return recommendations;
+        }
+        //Constructor
+        public RecommendationEngine(List<IRecommendationStrategy> strategies, List<IFilter> filters, IRankingStrategy ranking)
+        {
+            this.recommendationStrategies = strategies;
+            this.filters = filters;
+            this.rankingStrategy = ranking;
         }
     }
 }
